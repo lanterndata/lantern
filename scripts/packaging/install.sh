bf579beceb5c21aa6caa1fc5e4057db71ece1dc7
var77 <varikmatevosyan@gmail.com>
2023-07-31 02:10:02 +0400

    feat: test universal script

--- scripts/packaging/install.sh
+++ scripts/packaging/install.sh
@@ -10,11 +10,42 @@ then
   PG_CONFIG=$(which pg_config)
 fi
 
+if [ -z "$ARCH" ]
+then
+  if command -v dpkg &> /dev/null
+  then
+    ARCH=$(dpkg --print-architecture)
+  elif command -v apk &> /dev/null
+  then
+    ARCH=$(apk --print-arch)
+  elif command -v uname &> /dev/null
+  then
+    ARCH=$(uname -m)
+  else
+    echo "Could not detect system architecture. Please specify with ARCH env variable"
+    exit
+  fi
+fi
+
 PG_LIBRARY_DIR=$($PG_CONFIG --pkglibdir)
 PG_EXTENSION_DIR=$($PG_CONFIG --sharedir)/extension
+PG_VERSION_STRING=$($PG_CONFIG --version)
+PG_VERSION=$(echo $PG_VERSION_STRING | sed -E "s#^PostgreSQL ([0-9]+).*#\1#")
 
-cp -r src/*.so $PG_LIBRARY_DIR
-cp -r src/*.sql $PG_EXTENSION_DIR
-cp -r src/*.control $PG_EXTENSION_DIR
+if [ ! -d src/${ARCH} ]
+then
+  echo "Architecture $ARCH not supported. Try building from source"
+  exit
+fi
+
+if [ ! -d src/${ARCH}/${PG_VERSION} ]
+then
+  echo "Postgres version $PG_VERSION not supported"
+  exit
+fi
+
+cp -r src/${ARCH}/${PG_VERSION}/*.so $PG_LIBRARY_DIR
+cp -r shared/*.sql $PG_EXTENSION_DIR
+cp -r shared/*.control $PG_EXTENSION_DIR
 
 echo "LanternDB installed successfully"
