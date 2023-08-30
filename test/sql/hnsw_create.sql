be2e2f2b2ba0b4890116d3c5934aa90c6fbd5a95
Narek Galstyan <narekg@berkeley.edu>
2023-08-30 04:22:46 +0000

    Add empty table test for missing dimension

--- test/sql/hnsw_create.sql
+++ test/sql/hnsw_create.sql
@@ -37,6 +37,7 @@ CREATE TABLE small_world4 (
 );
 -- If the first row is NULL we do not infer a dimension
 \set ON_ERROR_STOP off
+CREATE INDEX ON small_world4 USING hnsw (vector) WITH (M=14, ef=22, ef_construction=2);
 begin;
 INSERT INTO small_world4 (id, vector) VALUES
 ('000', NULL),
