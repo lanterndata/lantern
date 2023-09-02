832dc0d157031f48a8e0f928da4be44515e25e05
yoloVoe <hi@subrat.info>
2023-09-02 15:22:23 -0600

    query cost: add current costs to tests
    Add log statements to hnsw.c so we can see a more detailed
    breakdown of current generic cost estimate.
    
    Also add hnsw_cost_estimate test so we can start
    capturing detailed cost estimate.
    
    test_runner.sh was also updated to ignore
    values that are non-deterministic.

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -40,5 +40,9 @@ psql -U ${DB_USER} \
      "$@" -d ${TEST_CASE_DB} 2>&1 | \
           sed  -e 's! Memory: [0-9]\{1,\}kB!!' \
                -e 's! Memory Usage: [0-9]\{1,\}kB!!' \
-               -e 's! Average  Peak Memory: [0-9]\{1,\}kB!!' | \
-          grep -v 'DEBUG:  rehashing catalog cache id'
+               -e 's! Average  Peak Memory: [0-9]\{1,\}kB!!' \
+               -e 's! (Buffers: shared hit=[0-9]\+) dirtied=[0-9]\+!!' \
+               -e 's! time=[0-9]\+\.[0-9]\+\.\.[0-9]\+\.[0-9]\+!!' | \
+          grep -v 'DEBUG:  rehashing catalog cache id' | \
+          grep -Gv '^ Planning Time:' | \
+          grep -Gv '^ Execution Time:'
