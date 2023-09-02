4274d8d65df4c281963d6f1770d2bdd419cd0fb9
yoloVoe <hi@subrat.info>
2023-09-02 15:49:54 -0600

    hnsw query cost estimate improvement
    First, estimate number of index tuples accessed.
    Then, based on that, estimate number of datablocks
    and blockmaps accessed.

--- test/sql/hnsw_cost_estimate.sql
+++ test/sql/hnsw_cost_estimate.sql
@@ -1,24 +1,53 @@
+SET client_min_messages=debug5;
+
 -- Goal: make sure query cost estimate is accurate
 -- when index is created with varying costruction parameters.
 
-CREATE TABLE sift_base10k (
+-- Case 0, sanity check. No data.
+CREATE TABLE sift_base10k_0 (
      id SERIAL PRIMARY KEY,
      v real[128]
 );
-
--- Case 1, simple: dims=128, M=2
-CREATE INDEX hnsw_idx ON sift_base10k USING hnsw (v dist_l2sq_ops) WITH (M=2, ef_construction=10, ef=4, dims=128);
-\copy sift_base10k (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
-SELECT V AS v4444  FROM sift_base10k WHERE id = 4444 \gset
-EXPLAIN (ANALYZE, BUFFERS) SELECT * FROM sift_base10k order by v <-> :'v4444'
+CREATE INDEX hnsw_idx ON sift_base10k_0 USING hnsw (v dist_l2sq_ops) WITH (M=2, ef_construction=10, ef=2, dims=2);
+EXPLAIN ANALYZE SELECT * FROM sift_base10k_0 order by v <-> '{1, 2}'
 LIMIT 10;
 DROP INDEX hnsw_idx;
 
--- Case 2, higher M: dims=128, M=20
--- Should see higher cost than Case 20.
-CREATE INDEX hnsw_idx ON sift_base10k USING hnsw (v dist_l2sq_ops) WITH (M=20, ef_construction=10, ef=4, dims=128);
-\copy sift_base10k (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
-SELECT V AS v4444  FROM sift_base10k WHERE id = 4444 \gset
-EXPLAIN (ANALYZE, BUFFERS) SELECT * FROM sift_base10k order by v <-> :'v4444'
+-- Case 1, more data in index.
+-- Should see higher cost than Case 0.
+CREATE TABLE sift_base10k_1 (
+     id SERIAL PRIMARY KEY,
+     v real[128]
+);
+\copy sift_base10k_1 (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
+CREATE INDEX hnsw_idx ON sift_base10k_1 USING hnsw (v dist_l2sq_ops) WITH (M=2, ef_construction=10, ef=4, dims=128);
+SELECT V AS v4444  FROM sift_base10k_1 WHERE id = 4444 \gset
+EXPLAIN ANALYZE SELECT * FROM sift_base10k_1 order by v <-> :'v4444'
+LIMIT 10;
+DROP INDEX hnsw_idx;
+
+-- Case 2, higher M.
+-- Should see higher cost than Case 1.
+CREATE TABLE sift_base10k_2 (
+     id SERIAL PRIMARY KEY,
+     v real[128]
+);
+\copy sift_base10k_2 (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
+CREATE INDEX hnsw_idx ON sift_base10k_2 USING hnsw (v dist_l2sq_ops) WITH (M=20, ef_construction=10, ef=4, dims=128);
+SELECT V AS v4444  FROM sift_base10k_2 WHERE id = 4444 \gset
+EXPLAIN ANALYZE SELECT * FROM sift_base10k_2 order by v <-> :'v4444'
+LIMIT 10;
+DROP INDEX hnsw_idx;
+
+-- Case 3, higher ef.
+-- Should see higher cost than Case 2.
+CREATE TABLE sift_base10k_3 (
+     id SERIAL PRIMARY KEY,
+     v real[128]
+);
+\copy sift_base10k_3 (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
+CREATE INDEX hnsw_idx ON sift_base10k_3 USING hnsw (v dist_l2sq_ops) WITH (M=20, ef_construction=10, ef=16, dims=128);
+SELECT V AS v4444  FROM sift_base10k_3 WHERE id = 4444 \gset
+EXPLAIN ANALYZE SELECT * FROM sift_base10k_3 order by v <-> :'v4444'
 LIMIT 10;
 DROP INDEX hnsw_idx;
