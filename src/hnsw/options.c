0ff568c40e847243690f44dbea46b4696b3434a5
Di Qi <di@lantern.dev>
2023-09-04 23:16:11 +0000

    Separate into new file

--- src/hnsw/options.c
+++ src/hnsw/options.c
@@ -1,20 +1,17 @@
 #include <postgres.h>
 
 #include "options.h"
-
+#include "parse_op.h"
 #include <access/htup_details.h>
 #include <access/reloptions.h>
 #include <catalog/pg_amproc.h>
 #include <catalog/pg_type_d.h>
-#include <parser/parse_oper.h>
 #include <parser/analyze.h>
-#include <nodes/makefuncs.h>
 #include <fmgr.h>
 #include <utils/catcache.h>
 #include <utils/guc.h>
 #include <utils/rel.h>  // RelationData
 #include <utils/syscache.h>
-#include <utils/lsyscache.h>
 
 #ifdef _WIN32
 #define access _access
@@ -137,129 +134,6 @@ bytea *ldb_amoptions(Datum reloptions, bool validate)
 
     return (bytea *)rdopts;
 #endif
-}
-
-static bool isOperatorUsedOutsideOrderBy(Node *node, List* oidList) {
-    if (node == NULL) return false;
-
-    if (IsA(node, TargetEntry)) {
-        TargetEntry *te = (TargetEntry *) node;
-        if (te->resjunk) {
-            return false;
-        }
-        return isOperatorUsedOutsideOrderBy(te->expr, oidList);
-    }
-
-    if (IsA(node, FuncExpr)) {
-        FuncExpr *funcExpr = (FuncExpr *) node;
-        ListCell *lc;
-        foreach(lc, funcExpr->args) {
-            if (isOperatorUsedOutsideOrderBy((Node *) lfirst(lc), oidList)) {
-                return true;
-            }
-        }
-    }
-    
-    // If it's a Query node, handle its main parts and recurse into its subqueries
-    if (IsA(node, Query)) {
-        Query *query = (Query *) node;
-
-        if (isOperatorUsedOutsideOrderBy((Node *) query->targetList, oidList)) {
-            return true;
-        }
-
-        // Recurse into RTEs that are subqueries
-        ListCell *lc;
-        foreach(lc, query->rtable) {
-            RangeTblEntry *rte = (RangeTblEntry *) lfirst(lc);
-            if (rte->rtekind == RTE_SUBQUERY) {
-                if (isOperatorUsedOutsideOrderBy((Node *) rte->subquery, oidList)) {
-                    return true;
-                }
-            }
-        }
-
-        foreach(lc, query->cteList) {
-            CommonTableExpr *cte = (CommonTableExpr *) lfirst(lc);
-            if (isOperatorUsedOutsideOrderBy(cte->ctequery, oidList)) {
-                return true;
-            }
-        }
-    }
-
-
-    if (IsA(node, OpExpr)) {
-        OpExpr *opExpr = (OpExpr *) node;
-        if (list_member_oid(oidList, opExpr->opno)) {
-            return true;
-        }
-    }
-    
-    // If it's a list, recurse into its items
-    if (IsA(node, List)) {
-        List *list = (List *) node;
-
-        ListCell *lc;
-        foreach(lc, list) {
-            if (isOperatorUsedOutsideOrderBy((Node *) lfirst(lc), oidList)) {
-                return true;
-            }
-        }
-    }
-    
-    if (IsA(node, ArrayExpr)) {
-        ArrayExpr *arrayExpr = (ArrayExpr *) node;
-        ListCell *lc;
-        foreach(lc, arrayExpr->elements) {
-            if (isOperatorUsedOutsideOrderBy((Node *) lfirst(lc), oidList)) {
-                return true;
-            }
-        }
-    }
-
-    if (IsA(node, SubLink)) {
-        SubLink *sublink = (SubLink *) node;
-        if (isOperatorUsedOutsideOrderBy(sublink->subselect, oidList)) {
-            return true;
-        }
-    }
-
-    if (IsA(node, CoalesceExpr)) {
-        CoalesceExpr *coalesce = (CoalesceExpr *)node;
-        ListCell *lc;
-        foreach(lc, coalesce->args) {
-            if (isOperatorUsedOutsideOrderBy(lfirst(lc), oidList)) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-List *
-get_operator_oids(ParseState *pstate)
-{
-    List *oidList = NIL;
-
-    Oid intArrayOid = get_array_type(INT4OID);
-    Oid floatArrayOid = get_array_type(FLOAT4OID);
-
-    List *nameList = lappend(NIL, makeString("<->"));
-
-    Oid intOperator = LookupOperName(pstate, nameList, intArrayOid, intArrayOid, true, -1);
-    Oid floatOperator = LookupOperName(pstate, nameList, floatArrayOid, floatArrayOid, true, -1);
-
-    if (OidIsValid(intOperator)) {
-        oidList = lappend_oid(oidList, intOperator);
-    }
-    if (OidIsValid(floatOperator)) {
-        oidList = lappend_oid(oidList, floatOperator);
-    }
-
-    list_free(nameList);
-
-    return oidList;
 }
 
 void post_parse_analyze_hook_with_operator_check(ParseState *pstate, Query *query
---
+++ src/hnsw/parse_op.c
@@ -0,0 +1,131 @@
+#include <postgres.h>
+
+#include "parse_op.h"
+#include <catalog/pg_type_d.h>
+#include <parser/parse_oper.h>
+#include <nodes/makefuncs.h>
+#include <utils/catcache.h>
+#include <utils/guc.h>
+
+static bool isOperatorUsedOutsideOrderBy(Node *node, List* oidList) {
+    if (node == NULL) return false;
+
+    if (IsA(node, TargetEntry)) {
+        TargetEntry *te = (TargetEntry *) node;
+        if (te->resjunk) {
+            return false;
+        }
+        return isOperatorUsedOutsideOrderBy(te->expr, oidList);
+    }
+
+    if (IsA(node, FuncExpr)) {
+        FuncExpr *funcExpr = (FuncExpr *) node;
+        ListCell *lc;
+        foreach(lc, funcExpr->args) {
+            if (isOperatorUsedOutsideOrderBy((Node *) lfirst(lc), oidList)) {
+                return true;
+            }
+        }
+    }
+    
+    // If it's a Query node, handle its main parts and recurse into its subqueries
+    if (IsA(node, Query)) {
+        Query *query = (Query *) node;
+
+        if (isOperatorUsedOutsideOrderBy((Node *) query->targetList, oidList)) {
+            return true;
+        }
+
+        // Recurse into RTEs that are subqueries
+        ListCell *lc;
+        foreach(lc, query->rtable) {
+            RangeTblEntry *rte = (RangeTblEntry *) lfirst(lc);
+            if (rte->rtekind == RTE_SUBQUERY) {
+                if (isOperatorUsedOutsideOrderBy((Node *) rte->subquery, oidList)) {
+                    return true;
+                }
+            }
+        }
+
+        foreach(lc, query->cteList) {
+            CommonTableExpr *cte = (CommonTableExpr *) lfirst(lc);
+            if (isOperatorUsedOutsideOrderBy(cte->ctequery, oidList)) {
+                return true;
+            }
+        }
+    }
+
+
+    if (IsA(node, OpExpr)) {
+        OpExpr *opExpr = (OpExpr *) node;
+        if (list_member_oid(oidList, opExpr->opno)) {
+            return true;
+        }
+    }
+    
+    // If it's a list, recurse into its items
+    if (IsA(node, List)) {
+        List *list = (List *) node;
+
+        ListCell *lc;
+        foreach(lc, list) {
+            if (isOperatorUsedOutsideOrderBy((Node *) lfirst(lc), oidList)) {
+                return true;
+            }
+        }
+    }
+    
+    if (IsA(node, ArrayExpr)) {
+        ArrayExpr *arrayExpr = (ArrayExpr *) node;
+        ListCell *lc;
+        foreach(lc, arrayExpr->elements) {
+            if (isOperatorUsedOutsideOrderBy((Node *) lfirst(lc), oidList)) {
+                return true;
+            }
+        }
+    }
+
+    if (IsA(node, SubLink)) {
+        SubLink *sublink = (SubLink *) node;
+        if (isOperatorUsedOutsideOrderBy(sublink->subselect, oidList)) {
+            return true;
+        }
+    }
+
+    if (IsA(node, CoalesceExpr)) {
+        CoalesceExpr *coalesce = (CoalesceExpr *)node;
+        ListCell *lc;
+        foreach(lc, coalesce->args) {
+            if (isOperatorUsedOutsideOrderBy(lfirst(lc), oidList)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+List *
+get_operator_oids(ParseState *pstate)
+{
+    List *oidList = NIL;
+
+    Oid intArrayOid = get_array_type(INT4OID);
+    Oid floatArrayOid = get_array_type(FLOAT4OID);
+
+    List *nameList = lappend(NIL, makeString("<->"));
+
+    Oid intOperator = LookupOperName(pstate, nameList, intArrayOid, intArrayOid, true, -1);
+    Oid floatOperator = LookupOperName(pstate, nameList, floatArrayOid, floatArrayOid, true, -1);
+
+    if (OidIsValid(intOperator)) {
+        oidList = lappend_oid(oidList, intOperator);
+    }
+    if (OidIsValid(floatOperator)) {
+        oidList = lappend_oid(oidList, floatOperator);
+    }
+
+    list_free(nameList);
+
+    return oidList;
+}
