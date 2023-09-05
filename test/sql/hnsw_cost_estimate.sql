f842b4ad2bf678697dc5afff8c8a8a542f6f5aba
Varik Matevosyan <varikmatevosyan@gmail.com>
2023-09-06 01:21:12 +0400

    Make tests more consistent

--- test/sql/hnsw_cost_estimate.sql
+++ test/sql/hnsw_cost_estimate.sql
@@ -9,7 +9,7 @@ CREATE TABLE sift_base10k_0 (
      v real[128]
 );
 CREATE INDEX hnsw_idx ON sift_base10k_0 USING hnsw (v dist_l2sq_ops) WITH (M=2, ef_construction=10, ef=2, dims=2);
-EXPLAIN ANALYZE SELECT * FROM sift_base10k_0 order by v <-> '{1, 2}'
+EXPLAIN (ANALYZE,TIMING FALSE) SELECT * FROM sift_base10k_0 order by v <-> '{1, 2}'
 LIMIT 10;
 DROP INDEX hnsw_idx;
 
@@ -22,7 +22,7 @@ CREATE TABLE sift_base10k_1 (
 \copy sift_base10k_1 (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
 CREATE INDEX hnsw_idx ON sift_base10k_1 USING hnsw (v dist_l2sq_ops) WITH (M=2, ef_construction=10, ef=4, dims=128);
 SELECT V AS v4444  FROM sift_base10k_1 WHERE id = 4444 \gset
-EXPLAIN ANALYZE SELECT * FROM sift_base10k_1 order by v <-> :'v4444'
+EXPLAIN (ANALYZE,TIMING FALSE) SELECT * FROM sift_base10k_1 order by v <-> :'v4444'
 LIMIT 10;
 DROP INDEX hnsw_idx;
 
@@ -35,7 +35,7 @@ CREATE TABLE sift_base10k_2 (
 \copy sift_base10k_2 (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
 CREATE INDEX hnsw_idx ON sift_base10k_2 USING hnsw (v dist_l2sq_ops) WITH (M=20, ef_construction=10, ef=4, dims=128);
 SELECT V AS v4444  FROM sift_base10k_2 WHERE id = 4444 \gset
-EXPLAIN ANALYZE SELECT * FROM sift_base10k_2 order by v <-> :'v4444'
+EXPLAIN (ANALYZE,TIMING FALSE) SELECT * FROM sift_base10k_2 order by v <-> :'v4444'
 LIMIT 10;
 DROP INDEX hnsw_idx;
 
@@ -48,6 +48,6 @@ CREATE TABLE sift_base10k_3 (
 \copy sift_base10k_3 (v) FROM '/tmp/lanterndb/vector_datasets/siftsmall_base_arrays.csv' with csv;
 CREATE INDEX hnsw_idx ON sift_base10k_3 USING hnsw (v dist_l2sq_ops) WITH (M=20, ef_construction=10, ef=16, dims=128);
 SELECT V AS v4444  FROM sift_base10k_3 WHERE id = 4444 \gset
-EXPLAIN ANALYZE SELECT * FROM sift_base10k_3 order by v <-> :'v4444'
+EXPLAIN (ANALYZE,TIMING FALSE) SELECT * FROM sift_base10k_3 order by v <-> :'v4444'
 LIMIT 10;
 DROP INDEX hnsw_idx;
