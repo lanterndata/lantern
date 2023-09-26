3b54578616335eeaf61c01d37bb1dbd30c150719
Varik Matevosyan <varikmatevosyan@gmail.com>
2023-09-26 11:49:45 +0400

    Fix op calsses for lantern_hnsw am

--- sql/lantern.sql
+++ sql/lantern.sql
@@ -1,8 +1,74 @@
 -- Definitions concerning our hnsw-based index data strucuture
-
 CREATE FUNCTION hnsw_handler(internal) RETURNS index_am_handler
 	AS 'MODULE_PATHNAME' LANGUAGE C;
 
+-- functions
+CREATE FUNCTION ldb_generic_dist(real[], real[]) RETURNS real
+	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+	
+CREATE FUNCTION ldb_generic_dist(integer[], integer[]) RETURNS real
+	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+	
+CREATE FUNCTION l2sq_dist(real[], real[]) RETURNS real
+	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION cos_dist(real[], real[]) RETURNS real
+	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION hamming_dist(integer[], integer[]) RETURNS integer
+	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+-- operators
+CREATE OPERATOR <-> (
+	LEFTARG = real[], RIGHTARG = real[], PROCEDURE = ldb_generic_dist,
+	COMMUTATOR = '<->'
+);
+
+CREATE OPERATOR <-> (
+	LEFTARG = integer[], RIGHTARG = integer[], PROCEDURE = ldb_generic_dist,
+	COMMUTATOR = '<->'
+);
+
+-- operator classes
+CREATE OR REPLACE FUNCTION _create_ldb_operator_classes(access_method_name TEXT) RETURNS BOOLEAN AS $$
+DECLARE
+    dist_l2sq_ops TEXT;
+    dist_cos_ops TEXT;
+    dist_hamming_ops TEXT;
+BEGIN
+    -- Construct the SQL statement to create the operator classes dynamically.
+    dist_l2sq_ops := '
+        CREATE OPERATOR CLASS dist_l2sq_ops
+        DEFAULT FOR TYPE real[] USING ' || access_method_name || ' AS
+        OPERATOR 1 <-> (real[], real[]) FOR ORDER BY float_ops,
+        FUNCTION 1 l2sq_dist(real[], real[]);
+    ';
+    
+    dist_cos_ops := '
+        CREATE OPERATOR CLASS dist_cos_ops
+        FOR TYPE real[] USING ' || access_method_name || ' AS
+        OPERATOR 1 <-> (real[], real[]) FOR ORDER BY float_ops,
+        FUNCTION 1 cos_dist(real[], real[]);
+    ';
+    
+    dist_hamming_ops := '
+        CREATE OPERATOR CLASS dist_hamming_ops
+        FOR TYPE integer[] USING ' || access_method_name || ' AS
+        OPERATOR 1 <-> (integer[], integer[]) FOR ORDER BY float_ops,
+        FUNCTION 1 hamming_dist(integer[], integer[]);
+    ';
+
+    -- Execute the dynamic SQL statement.
+    EXECUTE dist_l2sq_ops;
+    EXECUTE dist_cos_ops;
+    EXECUTE dist_hamming_ops;
+
+    RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
+
+
+-- Create access method
 DO $BODY$
 DECLARE
 	hnsw_am_exists boolean;
@@ -41,55 +107,14 @@ BEGIN
 
 
 	IF hnsw_am_exists THEN
+		PERFORM _create_ldb_operator_classes('lantern_hnsw');
 		RAISE WARNING 'Access method(index type) "hnsw" already exists. Creating lantern_hnsw access method';
 	ELSE
 		-- create access method
 		CREATE ACCESS METHOD hnsw TYPE INDEX HANDLER hnsw_handler;
 		COMMENT ON ACCESS METHOD hnsw IS 'LanternDB access method for vector embeddings, based on the hnsw algorithm';
+		PERFORM _create_ldb_operator_classes('hnsw');
 	END IF;
 END;
 $BODY$
 LANGUAGE plpgsql;
-
--- functions
-CREATE FUNCTION ldb_generic_dist(real[], real[]) RETURNS real
-	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-	
-CREATE FUNCTION ldb_generic_dist(integer[], integer[]) RETURNS real
-	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-	
-CREATE FUNCTION l2sq_dist(real[], real[]) RETURNS real
-	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
-CREATE FUNCTION cos_dist(real[], real[]) RETURNS real
-	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
-CREATE FUNCTION hamming_dist(integer[], integer[]) RETURNS integer
-	AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- operators
-CREATE OPERATOR <-> (
-	LEFTARG = real[], RIGHTARG = real[], PROCEDURE = ldb_generic_dist,
-	COMMUTATOR = '<->'
-);
-
-CREATE OPERATOR <-> (
-	LEFTARG = integer[], RIGHTARG = integer[], PROCEDURE = ldb_generic_dist,
-	COMMUTATOR = '<->'
-);
-
--- operator classes
-CREATE OPERATOR CLASS dist_l2sq_ops
-  DEFAULT FOR TYPE real[] USING hnsw AS
-	OPERATOR 1 <-> (real[], real[]) FOR ORDER BY float_ops,
-	FUNCTION 1 l2sq_dist(real[], real[]);
-
-CREATE OPERATOR CLASS dist_cos_ops
-	FOR TYPE real[] USING hnsw AS
-	OPERATOR 1 <-> (real[], real[]) FOR ORDER BY float_ops,
-	FUNCTION 1 cos_dist(real[], real[]);
-
-CREATE OPERATOR CLASS dist_hamming_ops
-	FOR TYPE integer[] USING hnsw AS
-	OPERATOR 1 <-> (integer[], integer[]) FOR ORDER BY float_ops,
-	FUNCTION 1 hamming_dist(integer[], integer[]);
