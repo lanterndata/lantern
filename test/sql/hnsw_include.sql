48001d9c81333a9ca4633a5e15fcbb07c00a01bc
Ezra Varady <ezraavarady@gmail.com>
2023-09-29 19:21:37 -1000

    update tests a bit

--- test/sql/hnsw_include.sql
+++ test/sql/hnsw_include.sql
@@ -21,6 +21,7 @@ EXPLAIN (COSTS FALSE) SELECT * FROM small_world ORDER BY v <-> '{1,1,1}' ASC;
 -- TODO this works but row ordering is non deterministic and a second order by forces seq scan
 -- SELECT n, v FROM small_world ORDER BY v <-> '{1,1,1}' ASC;
 
+-- build an index on an existing table
 DROP TABLE small_world;
 CREATE TABLE small_world (
     id int,
@@ -42,4 +43,13 @@ CREATE INDEX test ON small_world USING hnsw (v) INCLUDE (b) WITH (dim=3, M=5);
 EXPLAIN (COSTS FALSE) SELECT * FROM small_world ORDER BY v <-> '{1,1,1}' ASC;
 EXPLAIN (COSTS FALSE) SELECT b,v FROM small_world ORDER BY v <-> '{1,1,1}' ASC;
 SELECT b,v FROM small_world ORDER BY v <-> '{1,1,1}' ASC;
+-- modify the table
+UPDATE small_world SET v = '{1,1,1}' WHERE id = 1;
+DELETE FROM small_world WHERE id = 7;
+INSERT INTO small_world (id, b, v) VALUES (9, TRUE, '{1,1,1}');
+EXPLAIN (ANALYZE, COSTS FALSE) SELECT b,v FROM small_world ORDER BY v <-> '{1,1,1}' ASC;
+SELECT b,v FROM small_world ORDER BY v <-> '{1,1,1}' ASC LIMIT 3;
+-- vacuum to set visibility table
+VACUUM small_world;
+EXPLAIN (ANALYZE, COSTS FALSE) SELECT b,v FROM small_world ORDER BY v <-> '{1,1,1}' ASC;
 -- TODO add test for when included columns exceed page size
