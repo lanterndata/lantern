2baffc2a85db87762b1df7563df58a7ef6453eab
juuu-jiii <sc4020@rit.edu>
2023-10-09 15:02:01 +0800

    Create util testing scripts using unlogged tables

---
+++ test/sql/utils/small_world_array_unlogged.sql
@@ -0,0 +1,15 @@
+CREATE UNLOGGED TABLE small_world_unlogged (
+    id VARCHAR(3),
+    b BOOLEAN,
+    v REAL[3]
+);
+
+INSERT INTO small_world_unlogged (id, b, v) VALUES
+    ('000', TRUE,  '{0,0,0}'),
+    ('001', TRUE,  '{0,0,1}'),
+    ('010', FALSE, '{0,1,0}'),
+    ('011', TRUE,  '{0,1,1}'),
+    ('100', FALSE, '{1,0,0}'),
+    ('101', FALSE, '{1,0,1}'),
+    ('110', FALSE, '{1,1,0}'),
+    ('111', TRUE,  '{1,1,1}');
