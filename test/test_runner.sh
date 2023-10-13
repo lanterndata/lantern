05fbdf0faf16effe561eaca33e0315384cb42b11
Narek Galstyan <narekg@berkeley.edu>
2023-10-13 06:57:00 +0000

    Add logic to test runner to run update tests

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -27,42 +27,57 @@ function drop_db {
 EOF
 }
 
-# If these aren't parallel tests always drop the db after the test
-# if they are though we only want to drop after end which is where we check invariants
-# this allows the parallel tests to be run against the same db 
-if [ "$PARALLEL" -eq 0 ]; then
-    trap drop_db EXIT
-elif [[ "$TESTFILE_NAME" =~ ^end ]]; then
-    trap drop_db EXIT
-fi
+function run_regression_test {
+     # Exclude debug/inconsistent output from psql
+     # So tests will always have the same output
+     psql -U ${DB_USER} \
+          -v ON_ERROR_STOP=1 \
+          -v VERBOSITY=terse \
+          -v ECHO=all \
+          "$@" -d ${TEST_CASE_DB} 2>&1 | \
+               sed  -e 's! Memory: [0-9]\{1,\}kB!!' \
+                    -e 's! Memory Usage: [0-9]\{1,\}kB!!' \
+                    -e 's! Average  Peak Memory: [0-9]\{1,\}kB!!' \
+                    -e 's! time=[0-9]\+\.[0-9]\+\.\.[0-9]\+\.[0-9]\+!!' | \
+               grep -v 'DEBUG:  rehashing catalog cache id' | \
+               grep -Gv '^ Planning Time:' | \
+               grep -Gv '^ Execution Time:' | \
+               # Only print debug messages followed by LANTERN
+               perl -nle'print if !m{DEBUG:(?!.*LANTERN)}'
+}
+
+trap drop_db EXIT
 
 
-# Change directory to sql directory so sql imports will work correctly
+
+
+# Change directory to sql so sql imports will work correctly
 cd sql/
 
 # install lantern extension
-# if tests are parallel we only do this for the begin tests as we won't be dropping the database until the end
-# begin will handle initialization specific to the tests but expects the database already exists
-if [ "$PARALLEL" -eq 0 ] || ( [[ "$TESTFILE_NAME" =~ ^begin ]] && [ "$PARALLEL" -eq 1 ] ); then
-    psql "$@" -U ${DB_USER} -d postgres -v ECHO=none -q -c "DROP DATABASE IF EXISTS ${TEST_CASE_DB};" 2>/dev/null
-    psql "$@" -U ${DB_USER} -d postgres -v ECHO=none -q -c "CREATE DATABASE ${TEST_CASE_DB};" 2>/dev/null
-    psql "$@" -U ${DB_USER} -d ${TEST_CASE_DB} -v ECHO=none -q -c "SET client_min_messages=error; CREATE EXTENSION lantern;" 2>/dev/null
-    psql "$@" -U ${DB_USER} -d ${TEST_CASE_DB} -v ECHO=none -q -f utils/common.sql 2>/dev/null
-fi
+psql "$@" -U ${DB_USER} -d postgres -v ECHO=none -q -c "DROP DATABASE IF EXISTS ${TEST_CASE_DB};" 2>/dev/null
+psql "$@" -U ${DB_USER} -d postgres -v ECHO=none -q -c "CREATE DATABASE ${TEST_CASE_DB};" 2>/dev/null
+if [ ! -z "$UPDATE_EXTENSION" ]
+then
+     if [ -z "$UPDATE_FROM" ] || [ -z "$UPDATE_TO" ]
+     then
+          echo "ERROR: UPDATE_FROM and UPDATE_TO environment variables must be set before test_runner.sh whenever UPDATE_EXTENSION is set"
+          exit 1
+     fi
 
-# Exclude debug/inconsistent output from psql
-# So tests will always have the same output
-psql -U ${DB_USER} \
-     -v ON_ERROR_STOP=1 \
-     -v VERBOSITY=terse \
-     -v ECHO=all \
-     "$@" -d ${TEST_CASE_DB} 2>&1 | \
-          sed  -e 's! Memory: [0-9]\{1,\}kB!!' \
-               -e 's! Memory Usage: [0-9]\{1,\}kB!!' \
-               -e 's! Average  Peak Memory: [0-9]\{1,\}kB!!' \
-               -e 's! time=[0-9]\+\.[0-9]\+\.\.[0-9]\+\.[0-9]\+!!' | \
-          grep -v 'DEBUG:  rehashing catalog cache id' | \
-          grep -Gv '^ Planning Time:' | \
-          grep -Gv '^ Execution Time:' | \
-          # Only print debug messages followed by LANTERN
-          perl -nle'print if !m{DEBUG:(?!.*LANTERN)}'
+     # install the old version of the extension and sanity-check that all tests pass
+     psql "$@" -U ${DB_USER} -d ${TEST_CASE_DB} -v ECHO=none -q -c "SET client_min_messages=error; CREATE EXTENSION lantern VERSION '$UPDATE_FROM';" 2>/dev/null
+     psql "$@" -U ${DB_USER} -d ${TEST_CASE_DB} -v ECHO=none -q -f utils/common.sql 2>/dev/null
+     run_regression_test $@
+     # upgrade to the new version of the extension and make sure that all existing tests still pass
+     # todo:: this approach currently is broken for pgvector-compat related upgrade scripts as that regression test drops
+     # and recreates the extension so whatever we do here is ignored
+     psql "$@" -U ${DB_USER} -d ${TEST_CASE_DB} -v ECHO=none -q -c "SET client_min_messages=error; ALTER EXTENSION lantern UPDATE TO '$UPDATE_TO';" 2>/dev/null
+     run_regression_test $@
+else
+
+     psql "$@" -U ${DB_USER} -d ${TEST_CASE_DB} -v ECHO=none -q -c "SET client_min_messages=error; CREATE EXTENSION lantern;" 2>/dev/null
+     psql "$@" -U ${DB_USER} -d ${TEST_CASE_DB} -v ECHO=none -q -f utils/common.sql 2>/dev/null
+
+     run_regression_test $@
+fi
