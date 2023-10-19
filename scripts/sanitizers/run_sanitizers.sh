7a43d86f4184b61578530809038d2706476c607d
Ezra Varady <ezraavarady@gmail.com>
2023-10-19 10:08:58 -1000

    docker stuff working

--- scripts/sanitizers/run_sanitizers.sh
+++ scripts/sanitizers/run_sanitizers.sh
@@ -3,9 +3,19 @@ if [ ! -f src/hnsw.c ]; then
       echo "script must be run in lantern root directory"
       exit 1
 fi
-docker build -t lantern-san -f scripts/sanitizers/Dockerfile .
-docker run lantern-san
 
-docker exec -i -u postgres -w /lantern/build lantern-san /bin/bash <<EOF
+mkdir -p sanitizer
+
+docker build -t lantern-san -f scripts/sanitizers/Dockerfile .
+
+docker run --rm -d -v $(pwd)/sanitizer:/lantern/sanitizer --name lantern-sanitizers lantern-san
+
+docker exec -i -u root lantern-sanitizers /bin/bash <<EOF
+chown -R postgres:postgres /lantern/sanitizer
+EOF
+
+docker exec -i -u postgres -w /lantern/build lantern-sanitizers /bin/bash <<EOF
 make test
 EOF
+
+docker kill lantern-sanitizers
