c6d470f712c0a3776613b956196ffed38d297efe
Narek Galstyan <narekg@berkeley.edu>
2023-11-26 00:04:43 +0000

    Imporve db update script
    - Run `begin` with the old binary still loaded
    
    - Skip parallel tests in 0.0.4 as test-parallel target is not available
      yet
    
    - Fix target tag calculation

--- scripts/test_updates.py
+++ scripts/test_updates.py
@@ -28,7 +28,7 @@ def update_from_tag(from_version: str, to_version: str):
     repo.remotes[0].fetch()
     repo.git.checkout(from_tag)
     sha_after = repo.head.object.hexsha
-    print("sha_after", sha_after)
+    print(f"Updating from tag {from_tag}(sha: {sha_after}) to {to_version}")
 
     # run "mkdir build && cd build && cmake .. && make -j4 && make install"
     res = shell(f"mkdir -p {args.builddir} ; cd {args.builddir} && git submodule update && cmake .. && make -j4 && make install")
@@ -36,17 +36,22 @@ def update_from_tag(from_version: str, to_version: str):
     res = shell(f"psql postgres -U {args.user} -c 'DROP DATABASE IF EXISTS {args.db};'")
     res = shell(f"psql postgres -U {args.user} -c 'CREATE DATABASE {args.db};'")
     res = shell(f"psql postgres -U {args.user} -c 'DROP EXTENSION IF EXISTS lantern CASCADE; CREATE EXTENSION lantern;' -d {args.db};")
-    # todo:: run init() portion of parallel tests
+
+    # run begin of parallel tests. Run this while the from_tag version of the binary is installed and loaded
+    # run begin on {from_version}
+    if from_tag != "v0.0.4":
+        # the source code at 0.0.4 did not yet have parallel tests
+        res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={from_version} make test-parallel FILTER=begin")
 
     repo.git.checkout(sha_before)
     res = shell(f"cd {args.builddir} ; git submodule update && cmake .. && make -j4 && make install")
     res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={to_version} make test")
-    # run begin on {from_version}
-    res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={from_version} make test-parallel FILTER=begin")
     # run the actual parallel tests after the upgrade
     # todo: parallel tests are failing (tracked by https://github.com/lanterndata/lantern/issues/226)
     res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={to_version} make test-parallel EXCLUDE=begin", exit_on_error=False)
-    #todo:: run query and check portion of parallel tests
+
+    print(f"Update {from_version}->{to_version} Success!")
+
 
 def incompatible_version(pg_version, version_tag):
     if not pg_version or pg_version not in INCOMPATIBLE_VERSIONS:
@@ -82,10 +87,10 @@ if __name__ == "__main__":
 
     # test updates from all tags
     tag_pairs = [update_fname.split("--") for update_fname in os.listdir("sql/updates")]
-    from_tags = [p[0] for p in tag_pairs]
-    to_tags = [p[1].split(".sql")[0] for p in tag_pairs]
-    # os.listdir has alphabetical order and postgres enforces same order of versions
+    from_tags = list(sorted([p[0] for p in tag_pairs], reverse=True))
+    to_tags = list(sorted([p[1].split(".sql")[0] for p in tag_pairs]))
     latest_version = to_tags[-1]
+    print("Updating from tags", from_tags, "to ", latest_version)
 
     pg_version = None if not 'PG_VERSION' in os.environ else os.environ['PG_VERSION']
     for from_tag in from_tags:
