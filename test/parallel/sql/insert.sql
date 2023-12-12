ac8b0cc4653e4f909c63e32e9020bad20c47d234
Ezra Varady <ezraavarady@gmail.com>
2023-12-11 17:18:54 -1000

    reduce complexity to see if the error persists

--- test/parallel/sql/insert.sql
+++ test/parallel/sql/insert.sql
@@ -1,14 +1,5 @@
 SET work_mem='10MB';
 BEGIN;
 INSERT INTO sift_base10k (id, v) VALUES 
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128)),
-    (nextval('serial'), random_array(128, 0, 128));
+    (10001, random_array(128, 0, 128)),
 COMMIT;
