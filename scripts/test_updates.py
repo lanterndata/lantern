f3cc922b86be1744467e1438c0711e26dc3bee6b
Ezra Varady <ezraavarady@gmail.com>
2023-12-14 08:50:11 -1000

    fail if update test fails

--- scripts/test_updates.py
+++ scripts/test_updates.py
@@ -40,20 +40,17 @@ def update_from_tag(from_version: str, to_version: str):
     res = shell(f"psql postgres -U {args.user} -c 'DROP EXTENSION IF EXISTS lantern CASCADE; CREATE EXTENSION lantern;' -d {args.db};")
 
     # run begin of parallel tests. Run this while the from_tag version of the binary is installed and loaded run begin on {from_version}
-    if from_tag != "v0.0.4":
+    if from_tag == "v0.0.4":
         # the source code at 0.0.4 did not yet have parallel tests
-        res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={from_version} make test-parallel FILTER=begin")
-    else:
-        # if we aren't running begin the following tests won't behave correctly
         return
 
+    res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={from_version} make test-parallel FILTER=begin")
     repo.git.checkout(sha_before)
     res = shell(f"cd {args.builddir} ; git submodule update && cmake .. && make -j4 && make install")
     # res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={to_version} make test")
 
     # run the actual parallel tests after the upgrade
-    # todo: parallel tests are failing (tracked by https://github.com/lanterndata/lantern/issues/226)
-    res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={to_version} make test-parallel EXCLUDE=begin", exit_on_error=False)
+    res = shell(f"cd {args.builddir} ; UPDATE_EXTENSION=1 UPDATE_FROM={from_version} UPDATE_TO={to_version} make test-parallel EXCLUDE=begin")
 
     print(f"Update {from_version}->{to_version} Success!")
 
