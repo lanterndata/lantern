f59bffeb9f59d5c7a39464a03fa1a35a5f97104a
Ezra Varady <ezraavarady@gmail.com>
2023-12-17 00:05:36 -1000

    replace >> with >

--- scripts/version_header.sh
+++ scripts/version_header.sh
@@ -5,7 +5,7 @@ OUTPUT=$2
 
 mkdir -p ${OUTPUT}/include
 
-cat << EOF >> ${OUTPUT}/include/version.h
+cat << EOF > ${OUTPUT}/include/version.h
 #ifndef LDB_HNSW_VERSION_H
 #define LDB_HNSW_VERSION_H
 
