834a602400e5eebf8e854a8da8c8fd8a1c75c355
Danyil <danyilblyschak@gmail.com>
2023-12-23 16:53:40 -0500

    added manual test cases where we switch from logged/unlogged

--- test/sql/manual_tests/hnsw_unlogged_post.sql
+++ test/sql/manual_tests/hnsw_unlogged_post.sql
@@ -6,12 +6,16 @@ SELECT _lantern_internal.validate_index('unlogged_world1_hnsw_idx', true);
 --SELECT _lantern_internal.validate_index('unlogged_world2_hnsw_idx', true); 
 SELECT _lantern_internal.validate_index('unlogged_world3_hnsw_idx', true); 
 SELECT _lantern_internal.validate_index('unlogged_world4_hnsw_idx', true); 
+SELECT _lantern_internal.validate_index('morph_world_hnsw_idx', true); 
+SELECT _lantern_internal.validate_index('morph_world2_hnsw_idx', true); 
 
 -- Verify that the tables are now in fact empty after the crash, since tables are unlogged 
 SELECT * from unlogged_world1;
 --SELECT * from unlogged_world2;
 SELECT * from unlogged_world3;
 SELECT * from unlogged_world4;
+SELECT * from morph_world;
+SELECT * from morph_world2;
 
 -- Verify that the indexes are operational
 set enable_seqscan = false;
@@ -30,15 +34,25 @@ SELECT * FROM unlogged_world3 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10;
 EXPLAIN SELECT * FROM unlogged_world4 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10;
 SELECT * FROM unlogged_world4 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
 
+EXPLAIN SELECT * FROM morph_world ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10;
+SELECT * FROM morph_world ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
+
+EXPLAIN SELECT * FROM morph_world2 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10;
+SELECT * FROM morph_world2 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
+
 -- Insert data into each one 
 INSERT INTO unlogged_world1 (id, vector) VALUES ('101', '{1,2,3,4}');
 --INSERT INTO unlogged_world2 (id, vector) VALUES ('101', '{1,2,3,4}');
 INSERT INTO unlogged_world3 (id, vector) VALUES ('101', '{1,2,3,4}');
 INSERT INTO unlogged_world4 (id, vector) VALUES ('101', '{1,2,3,4}');
+INSERT INTO morph_world (id, vector) VALUES ('101', '{1,2,3,4}');
+INSERT INTO morph_world2 (id, vector) VALUES ('101', '{1,2,3,4}');
+
 
 -- Test queries after new data inserted
 SELECT * FROM unlogged_world1 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
 --SELECT * FROM unlogged_world2 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
 SELECT * FROM unlogged_world3 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
 SELECT * FROM unlogged_world4 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
-
+SELECT * FROM morph_world ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
+SELECT * FROM morph_world2 ORDER BY vector <-> ARRAY[0, 0, 0, 0] LIMIT 10; 
--- test/sql/manual_tests/hnsw_unlogged_pre.sql
+++ test/sql/manual_tests/hnsw_unlogged_pre.sql
@@ -13,6 +13,9 @@ DROP TABLE IF EXISTS unlogged_world4;
 -- unlogged_world3: non-empty, dimension specified in index
 -- unlogged_world4: non-empty, dimension not specified in index
 
+-- morph_world: will start as unlogged and then be altered to logged; non-empty, dimension not specified 
+-- morph_world2: will start as logged and then be altered to unlogged; non-empty, dimension not specified 
+
 
 CREATE UNLOGGED TABLE unlogged_world1 (
     id varchar(3),
@@ -32,6 +35,16 @@ CREATE UNLOGGED TABLE unlogged_world3 (
 );
 
 CREATE UNLOGGED TABLE unlogged_world4 (
+    id varchar(3),
+    vector real[]
+);
+
+CREATE UNLOGGED TABLE morph_world (
+    id varchar(3),
+    vector real[]
+);
+
+CREATE TABLE morph_world2 (
     id varchar(3),
     vector real[]
 );
@@ -58,17 +71,50 @@ INSERT INTO unlogged_world4 (id, vector) VALUES
 ('110', '{1,1,1,0}'),
 ('111', '{1,1,1,1}');
 
+INSERT INTO morph_world (id, vector) VALUES
+('000', '{1,0,0,0}'),
+('001', '{1,0,0,1}'),
+('010', '{1,0,1,0}'),
+('011', '{1,0,1,1}'),
+('100', '{1,1,0,0}'),
+('101', '{1,1,0,1}'),
+('110', '{1,1,1,0}'),
+('111', '{1,1,1,1}');
+
+INSERT INTO morph_world2 (id, vector) VALUES
+('000', '{1,0,0,0}'),
+('001', '{1,0,0,1}'),
+('010', '{1,0,1,0}'),
+('011', '{1,0,1,1}'),
+('100', '{1,1,0,0}'),
+('101', '{1,1,0,1}'),
+('110', '{1,1,1,0}'),
+('111', '{1,1,1,1}');
+
+
+-- Change table status
+ALTER TABLE morph_world SET LOGGED;
+
+ALTER TABLE morph_world2 SET UNLOGGED;
+
+
 -- Verify contents of unlogged tables pre-crash
 SELECT * from unlogged_world1;
 --SELECT * from unlogged_world2;
 SELECT * from unlogged_world3;
 SELECT * from unlogged_world4;
+SELECT * from morph_world;
+SELECT * from morph_world2;
+
 
 -- Create indexes
 CREATE INDEX unlogged_world1_hnsw_idx ON unlogged_world1 USING hnsw (vector) WITH (M=14, ef=22, ef_construction=2, dim=4);
 --CREATE INDEX unlogged_world2_hnsw_idx ON unlogged_world2 USING hnsw (vector) WITH (M=14, ef=22, ef_construction=2);
 CREATE INDEX unlogged_world3_hnsw_idx ON unlogged_world3 USING hnsw (vector) WITH (M=14, ef=22, ef_construction=2, dim=4);
 CREATE INDEX unlogged_world4_hnsw_idx ON unlogged_world4 USING hnsw (vector) WITH (M=14, ef=22, ef_construction=2);
+CREATE INDEX morph_world_hnsw_idx ON morph_world USING hnsw (vector) WITH (M=14, ef=22, ef_construction=2);
+CREATE INDEX morph_world2_hnsw_idx ON morph_world2 USING hnsw (vector) WITH (M=14, ef=22, ef_construction=2);
+
 
 
 -- Validate indexes pre-crash
@@ -76,6 +122,9 @@ SELECT _lantern_internal.validate_index('unlogged_world1_hnsw_idx', true);
 --SELECT _lantern_internal.validate_index('unlogged_world2_hnsw_idx', true);
 SELECT _lantern_internal.validate_index('unlogged_world3_hnsw_idx', true);
 SELECT _lantern_internal.validate_index('unlogged_world4_hnsw_idx', true);
+SELECT _lantern_internal.validate_index('morph_world_hnsw_idx', true);
+SELECT _lantern_internal.validate_index('morph_world2_hnsw_idx', true);
+
 
 
 -- Now, we crash the database (todo:: find a way to do this programatically from within this .sql file?)
