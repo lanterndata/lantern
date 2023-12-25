50e65778424ccd2f29e6cf499b30c23e2cc5039d
Varik Matevosyan <varikmatevosyan@gmail.com>
2023-12-25 11:50:44 +0400

    Remove unnecessary initialization

--- src/hnsw/build.c
+++ src/hnsw/build.c
@@ -423,7 +423,7 @@ static void BuildIndex(
     // size of static name + max digits of uint32 (Oid) 10 + 1 for nullbyte and - 2 for %d format specifier
     const uint32       tmp_index_file_char_cnt = strlen(tmp_index_file_fmt_str) + 9;
     int                index_file_fd;
-    int                munmap_ret = 0;
+    int                munmap_ret;
     usearch_metadata_t metadata;
     size_t             num_added_vectors;
 
