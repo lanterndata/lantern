0835487b64da3bc611d55c648734bd4e1982e30d
Narek Galstyan <narekg@berkeley.edu>
2024-01-29 08:03:28 +0000

    Fix file-related tests for storage upgrade

--- test/sql/hnsw_todo.sql
+++ test/sql/hnsw_todo.sql
@@ -50,7 +50,7 @@ INSERT INTO sift_base1k (id, v) VALUES
 (1001, array_fill(1, ARRAY[128])),
 (1102, array_fill(2, ARRAY[128]));
 SELECT v AS v1001 FROM sift_base1k WHERE id = 1001 \gset
-CREATE INDEX hnsw_l2_index ON sift_base1k USING hnsw (v) WITH (_experimental_index_path='/tmp/lantern/files/index-sift1k-l2.usearch');
+CREATE INDEX hnsw_l2_index ON sift_base1k USING hnsw (v) WITH (_experimental_index_path='/tmp/lantern/files/index-sift1k-l2sq-0.0.13.usearch');
 SELECT _lantern_internal.validate_index('hnsw_l2_index', false);
 -- The 1001 and 1002 vectors will be ignored in search, so the first row will not be 0 in result
 SELECT ROUND(l2sq_dist(v, :'v1001')::numeric, 2) FROM sift_base1k order by v <?> :'v1001' LIMIT 1;
@@ -62,7 +62,7 @@ SELECT ROUND(l2sq_dist(v, :'v1001')::numeric, 2) FROM sift_base1k order by v <?>
 DROP TABLE sift_base1k CASCADE;
 \ir utils/sift1k_array.sql
 UPDATE sift_base1k SET v=:'v1001' WHERE id=777;
-CREATE INDEX hnsw_l2_index ON sift_base1k USING hnsw (v) WITH (_experimental_index_path='/tmp/lantern/files/index-sift1k-l2.usearch');
+CREATE INDEX hnsw_l2_index ON sift_base1k USING hnsw (v) WITH (_experimental_index_path='/tmp/lantern/files/index-sift1k-l2sq-0.0.13.usearch');
 SELECT _lantern_internal.validate_index('hnsw_l2_index', false);
 -- The first row will not be 0 now as the vector under id=777 was updated to 1,1,1,1... but it was indexed with different vector
 -- So the usearch index can not find 1,1,1,1,1.. vector in the index and wrong results will be returned
