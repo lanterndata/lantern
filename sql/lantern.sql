192a4f2a0c4ad5def332d60a79d6949ff0fb658b
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-02-08 15:50:31 +0400

    Reindex external indexes properly on updates #282

--- sql/lantern.sql
+++ sql/lantern.sql
@@ -185,11 +185,15 @@ RETURNS VOID AS $$
 DECLARE
     r RECORD;
 BEGIN
-    FOR r IN SELECT indexname FROM pg_indexes
+    FOR r IN SELECT indexname, indexdef FROM pg_indexes
             WHERE indexdef ILIKE '%USING hnsw%' OR indexdef ILIKE '%USING lantern_hnsw%'
     LOOP
         RAISE NOTICE 'Reindexing index: %', r.indexname;
-        EXECUTE 'REINDEX INDEX ' || quote_ident(r.indexname) || ';';
+        IF POSITION('_experimental_index_path' in r.indexdef) > 0 THEN
+          PERFORM lantern_reindex_external_index(r.indexname::regclass);
+        ELSE
+          EXECUTE 'REINDEX INDEX ' || quote_ident(r.indexname) || ';';
+        END IF;
         RAISE NOTICE 'Reindexed index: %', r.indexname;
     END LOOP;
 END $$ LANGUAGE plpgsql VOLATILE;
