3d15dbf94887a755af4a9d91de48d2684ead4c45
Narek Galstyan <narekg@berkeley.edu>
2024-03-11 00:47:09 +0000

    Fix outdated access method name in test

--- test/misc/sql/version_mismatch.sql
+++ test/misc/sql/version_mismatch.sql
@@ -1,2 +1,2 @@
 -- Validate that creating an index with mismatched versions fails
-CREATE INDEX ON sift_base1k USING hnsw (v) WITH (dim=128, M=4);
+CREATE INDEX ON sift_base1k USING lantern_hnsw (v) WITH (dim=128, M=4);
