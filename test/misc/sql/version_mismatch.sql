0583b8fa0578f31f339201455c5140462b65efa9
Narek Galstyan <narekg@berkeley.edu>
2024-03-11 01:13:24 -0700

    Fix snapshot leak bug and initialize parameters in pg_init before using them (#296)
    * Initialize parameters in pg_init before using them
    
    * Change dev docker image to reproduce snapshot leak outside of production
    
    * Fix snapshot leak bug
    
    In testing we never had other libraries that modified the ExecutorStart
    hook.
    As a result we did not catch the bug that got triggered only when were
    not the only one modifying the hook.
    This commit reproduces the issue in testing and fixes it.
    
    The problem was that we were calling the original executor start
    hook even when there was another hook present, loaded by another
    extension.
    
    But the right approach is to only call that other hook and assumes
    that hook itself will call the standard hook down the chain.
    
    See pg_stat_statements implementation of the relevant section here:
    Lines 991-994:
    https://doxygen.postgresql.org/pg__stat__statements_8c.html#af004be8a1c6880821a28272be9a2b205
    
    * Disable old update tests
    
    * Fix outdated access method name in test
    
    * Suppress version mismatch warning

--- test/misc/sql/version_mismatch.sql
+++ test/misc/sql/version_mismatch.sql
@@ -1,2 +1,6 @@
 -- Validate that creating an index with mismatched versions fails
-CREATE INDEX ON sift_base1k USING hnsw (v) WITH (dim=128, M=4);
+
+-- There is a warning that prints specific versions in the mismatch
+-- We do not want that for regression tests
+SET client_min_messages=ERROR;
+CREATE INDEX ON sift_base1k USING lantern_hnsw (v) WITH (dim=128, M=4);
