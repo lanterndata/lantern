b140eb5adee6fb94afb9a9a1495d9bde8e20b981
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-03-11 11:53:35 +0400

    Fix pg11 build issue on CI

--- ci/scripts/build-mac.sh
+++ ci/scripts/build-mac.sh
@@ -13,9 +13,9 @@ function setup_postgres() {
   then
     # Runner is github CI user
     sh -c "sudo -u runner -i $cmd"
-    sh -c "sudo -u runner -i brew reinstall gettext"
-    sh -c "sudo -u runner -i brew unlink gettext"
-    sh -c "sudo -u runner -i brew link gettext --force"
+    sh -c "sudo -u runner -i brew reinstall gettext || true"
+    sh -c "sudo -u runner -i brew unlink gettext || true"
+    sh -c "sudo -u runner -i brew link gettext --force || true"
   else
     sh -c $cmd
   fi
