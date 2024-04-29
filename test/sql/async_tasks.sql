8b1cbea6cb65df1fb7d0b1e81d547e1f06a21226
Narek Galstyan <narekg@berkeley.edu>
2024-04-29 00:54:24 +0000

    Reproduce bug on jobid mismatch between lantern.tasks and pg_cron
    Recreates the bug where succeeded lantern tasks would not have their
    status updated because of a bad update query that confused
    lantern.task's jobid column and cron's jobid column

--- test/sql/async_tasks.sql
+++ test/sql/async_tasks.sql
@@ -20,6 +20,7 @@ SELECT pg_sleep(3);
 SELECT jobid, query, pg_cron_job_name, job_name, duration IS NOT NULL AS is_done, status, error_message FROM lantern.tasks;
 SELECT lantern.cancel_all_async_tasks();
 
+
 -- test async tasks on index creation
 
 DROP TABLE IF EXISTS small_world;
@@ -58,5 +59,11 @@ SELECT lantern.async_task($$CREATE INDEX idx2 ON "sift_base1k_UpperCase" USING l
 -- this should fail since test_user does not have permission to drop the table
 -- sql line for do not stop on error
 SELECT lantern.async_task('DROP TABLE "sift_base1k_UpperCase";', 'Dropping Table Job');
+
+-- lantern.tasks jobid is disitinct and independent from cron.jobid, even though they may often overlap
+-- make sure everything works even when they are out of sync
+SELECT nextval('lantern.tasks_jobid_seq');
+SELECT lantern.async_task($$SELECT 42$$, 'Life');
+
 SELECT pg_sleep(4);
 SELECT jobid, query, pg_cron_job_name, job_name, duration IS NOT NULL AS is_done, status, error_message FROM lantern.tasks ORDER BY jobid;
