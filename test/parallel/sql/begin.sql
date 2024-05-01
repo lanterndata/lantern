005dc720d23b2026a50c33283e1d5df1f1a2ac43
Narek Galstyan <narekg@berkeley.edu>
2024-05-01 21:39:21 +0000

    Add tests for concurrent updates across versions

--- test/parallel/sql/begin.sql
+++ test/parallel/sql/begin.sql
@@ -1,6 +1,13 @@
 -- This file handles initializing the database before parallel tests are run
+ALTER SYSTEM SET work_mem = '128MB';
+SELECT pg_reload_conf();
+
 \ir utils/sift10k_array.sql
 \ir utils/random_array.sql
 
 CREATE SEQUENCE serial START 10001;
-CREATE INDEX ON sift_base10k  USING lantern_hnsw (v) WITH (M=5, ef=20, ef_construction=20);
+SELECT v as v444 from sift_base10k wHERE id = 444 LIMIT 1 \gset
+SELECT id, ROUND((v <-> :'v444')::numeric, 2) FROM sift_base10k ORDER BY v <-> :'v444' LIMIT 6;
+CREATE INDEX to_be_reindexed ON sift_base10k  USING lantern_hnsw (v) WITH (M=7, ef=20, ef_construction=20);
+SELECT id, ROUND((v <-> :'v444')::numeric, 2) FROM sift_base10k ORDER BY v <-> :'v444' LIMIT 6;
+REINDEX INDEX CONCURRENTLY to_be_reindexed;
