3e83c6112ad8ea6f3cee5beadbe4777ae2afbe32
Narek Galstyan <narekg@berkeley.edu>
2024-05-08 10:04:55 -0700

    Move to using an anonymous namespace in usearch_storage.cpp

--- src/hnsw/usearch_storage.cpp
+++ src/hnsw/usearch_storage.cpp
@@ -31,8 +31,6 @@ uint32_t UsearchNodeBytes(const metadata_t *metadata, int vector_bytes, int leve
 void usearch_init_node(
     metadata_t *meta, char *tape, unsigned long key, uint32_t level, uint32_t slot_id, void *vector, size_t vector_len)
 {
-    using namespace unum::usearch;
-    using node_t = unum::usearch::node_at<default_key_t, lantern_slot_t>;
     int node_size = UsearchNodeBytes(meta, vector_len, level);
     std::memset(tape, 0, node_size);
     node_t node = node_t{tape};
@@ -48,8 +46,6 @@ char *extract_node(char              *data,
                    /*->>output*/ int *node_size,
                    int               *level)
 {
-    using namespace unum::usearch;
-    using node_t = unum::usearch::node_at<default_key_t, lantern_slot_t>;
     char  *tape = data + progress;
     node_t node = node_t{tape};
 
@@ -61,16 +57,12 @@ char *extract_node(char              *data,
 
 unsigned long label_from_node(char *node)
 {
-    using namespace unum::usearch;
-    using node_t = unum::usearch::node_at<default_key_t, lantern_slot_t>;
     node_t n = node_t{node};
     return n.key();
 }
 
 void reset_node_label(char *node)
 {
-    using namespace unum::usearch;
-    using node_t = unum::usearch::node_at<default_key_t, lantern_slot_t>;
     node_t n = node_t{node};
     n.key(INVALID_ELEMENT_LABEL);
 }
