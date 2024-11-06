b494a681df6b6c8b14b251ea217bc3df704dc76b
Narek Galstyan <narekg@berkeley.edu>
2024-11-05 17:25:39 -0800

    Add bm25 approximation threshhold guc

--- lantern_extras/src/lib.rs
+++ lantern_extras/src/lib.rs
@@ -37,6 +37,8 @@ pub static ENABLE_INDEXING_SERVER: GucSetting<bool> = GucSetting::<bool>::new(tr
 pub static DAEMON_DATABASES: GucSetting<Option<&'static CStr>> =
     GucSetting::<Option<&'static CStr>>::new(None);
 
+pub static BM25_DEFAULT_APPROXIMATION_THRESHHOLD: GucSetting<i32> = GucSetting::<i32>::new(8000);
+
 #[allow(non_snake_case)]
 #[pg_guard]
 pub unsafe extern "C" fn _PG_init() {
@@ -108,6 +110,14 @@ pub unsafe extern "C" fn _PG_init() {
         "Flag to indicate if local indexing server is enabled or not",
         &ENABLE_INDEXING_SERVER,
         GucContext::Sighup,
+    GucRegistry::define_int_guc(
+        "lantern_extras.bm25_default_approximation_threshhold",
+        "Term popularity threashold, after which approximation is used",
+        "Term popularity threashold, after which approximation is used",
+        &BM25_DEFAULT_APPROXIMATION_THRESHHOLD,
+        5000,
+        100_000,
+        GucContext::Userset,
         GucFlags::NO_SHOW_ALL,
     );
 }
